f92bb83f7125edf9bbabc3ddd5f414c7af5bda1c
Gustavo Soares <soaresgu2710@gmail.com>
2025-01-29 10:53:38 -0300

    added message parsing

--- lib/message.ml
+++ lib/message.ml
@@ -1,10 +1,5 @@
 open Core
 
-type pattern_values =
-  | Text of string
-  | Variable of (string * string)  (** name : value *)
-  | UserMessage of string
-
 module Scanner = struct
   type token = Lbrace | Rbrace | Value of string | Text of string
   [@@deriving show]
@@ -36,9 +31,30 @@ module Scanner = struct
           match ch with
           | '{' -> add_token s Lbrace |> next
           | '}' -> add_token s Rbrace |> next
-          | c when is_text c ->
-              let scanner, token = read_string s in
-              add_token scanner token
+          | c when is_text c -> (
+              let scanner, data = read_string s in
+              let result =
+                match List.hd scanner.tokens with
+                | Some token -> (
+                    match token with
+                    | Lbrace -> Ok (Value data)
+                    | Value v ->
+                        let err =
+                          {
+                            at = scanner.pos;
+                            cause =
+                              Format.sprintf
+                                "invalid pattern syntax ::: %s followed by %s" v
+                                data;
+                          }
+                        in
+                        Error err
+                    | Rbrace | Text _ -> Ok (Text data))
+                | None -> Ok (Text data)
+              in
+              match result with
+              | Ok tok -> add_token scanner tok
+              | Error e -> add_error scanner e)
           | c ->
               {
                 at = s.pos;
@@ -71,7 +87,76 @@ module Scanner = struct
     let chopped_data =
       String.sub s.pattern ~pos:init_pos ~len:(end_pos - init_pos)
     in
-    (scanner, Text chopped_data)
+    (scanner, chopped_data)
 end
 
-module Parser = struct end
+module Parser = struct
+  type error_position = { tok : Scanner.token; pos : int } [@@deriving show]
+  type error = { at : error_position; cause : string } [@@deriving show]
+
+  type pattern_values =
+    | Text of string
+    | Variable of string
+    | UserMessage of string
+  [@@deriving show]
+
+  type t = {
+    variables : (string * string) list;
+    template : Config.template;
+    tokens : Scanner.token list;
+    values : pattern_values list;
+    errors : error list;
+    pos : int;
+    actual : Scanner.token option;
+  }
+  [@@deriving show]
+
+  let init tokens (config : Config.t) =
+    let tokens = List.rev tokens in
+    {
+      tokens;
+      values = [];
+      variables = config.variables;
+      template = config.template;
+      errors = [];
+      actual = List.nth tokens 0;
+      pos = 0;
+    }
+
+  let rec parse p =
+    match p.actual with
+    | Some tk ->
+        let parser =
+          match tk with
+          | Lbrace -> next p
+          | Rbrace -> next p
+          | Text t -> add_text p t |> next
+          | Value v -> (
+              let variable =
+                List.find p.variables ~f:(fun var ->
+                    let name, _ = var in
+                    String.equal name v)
+              in
+              match variable with
+              | Some (_, value) -> add_variable p value |> next
+              | None ->
+                  {
+                    at = { tok = tk; pos = p.pos };
+                    cause = Format.sprintf "Unknown variable with name '%s'" v;
+                  }
+                  |> add_error p |> next)
+        in
+        parse parser
+    | None -> p
+
+  and add_variable p v = { p with values = Variable v :: p.values }
+  and add_text p t = { p with values = Text t :: p.values }
+  and add_error p err = { p with errors = err :: p.errors }
+  and has_errors p = List.length p.errors > 0
+
+  and next ?(skip = 1) p =
+    if p.pos + skip >= List.length p.tokens - 1 then { p with actual = None }
+    else
+      let position = p.pos + skip in
+      { p with pos = position; actual = List.nth p.tokens position }
+end
--- lib/message.mli
+++ lib/message.mli
@@ -1,6 +1,36 @@
 module Scanner : sig
-  type t [@@deriving show]
+  type token [@@deriving show]
+  type error [@@deriving show]
+
+  type t = {
+    pattern : string;
+    tokens : token list;
+    errors : error list;
+    ch : char option;
+    pos : int;
+  }
+  [@@deriving show]
 
   val init : string -> t
   val scan : t -> t
+end
+
+module Parser : sig
+  type error_position [@@deriving show]
+  type error [@@deriving show]
+  type pattern_values [@@deriving show]
+
+  type t = {
+    variables : (string * string) list;
+    template : Config.template;
+    tokens : Scanner.token list;
+    values : pattern_values list;
+    errors : error list;
+    pos : int;
+    actual : Scanner.token option;
+  }
+  [@@deriving show]
+
+  val init : Scanner.token list -> Config.t -> t
+  val parse : t -> t
 end
