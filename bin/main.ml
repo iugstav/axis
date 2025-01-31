ef83514df100d2a7188de673cc557d944934e671
Gustavo Soares <soaresgu2710@gmail.com>
2025-01-31 16:40:17 -0300

    added support for unix output pipe

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,15 @@
 open Axis
 open Core
 
+(* lembrar de executar git commit -F - pra poder pegar o valor do pipe *)
+let output s =
+  let open Out_channel in
+  let is_output = not (Core_unix.isatty Core_unix.stdout) in
+  if is_output then (
+    output_line stdout s;
+    flush stdout)
+  else print_endline s
+
 let get_yaml_config templ yaml =
   match Yaml.of_string yaml with
   | Ok data -> Config.parse_yaml_data templ data
@@ -21,14 +30,14 @@ let run template_name user_message =
         exit 1
   in
   if List.is_empty result.errors then
-    Message.Parser.build result user_message |> print_endline
-  else Message.Parser.parse_errors result.errors |> List.iter ~f:print_endline
+    Message.Parser.build result user_message |> output
+  else Message.Parser.parse_errors result.errors |> List.iter ~f:output
 
 let cmd =
   let open Commands in
   let open Cmdliner in
   let info =
-    Cmd.info "axis" ~version:"1.0.0"
+    Cmd.info "axis" ~version:"0.0.1"
       ~doc:
         "formats your commit message based on your templates in the \
          configuration file"
