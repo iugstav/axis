3410921ec65c17fe0bcbda4f3528c8a328ccf9d8
Gustavo Soares <soaresgu2710@gmail.com>
2025-01-31 16:39:16 -0300

    better support for commands

--- lib/commands.ml
+++ lib/commands.ml
@@ -2,8 +2,8 @@ open Cmdliner
 
 let templ_name =
   let doc = "name of the template" in
-  Arg.(required & pos 1 (some string) None (info [] ~doc))
+  Arg.(required & pos 0 (some string) None (info [] ~docv:"NAME" ~doc))
 
 let user_message =
   let doc = "message to insert in the template" in
-  Arg.(required & pos 2 (some string) None (info [] ~doc))
+  Arg.(required & pos 1 (some string) None (info [] ~docv:"MESSAGE" ~doc))
